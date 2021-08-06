25faf4c679a4dc9167ecdc47787d008904d40241
Farooq Alwi <farooqalwi313@gmail.com>
2021-08-06 20:28:35 +0500

    working on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,39 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 const postcss = require('gulp-postcss');
+const postcssPresetEnv = require('postcss-preset-env');
 
-exports.default = function() {
-  return src('./themes/simple/static/**/*.css')
-  .pipe(postcss())
-    .pipe(dest('output/'));
-}
+
+const watchFiles = () => {
+    watch(
+        [
+            "./themes/simple/static/**/*.css"
+        ],
+        { ignoreInitial: false },
+        buildAll
+    );
+};
+
+const compileCSS = () => {
+    const plugins = [
+        // postcssPresetEnv comes with autoprefixer
+        postcssPresetEnv({ stage: 1 })
+    ];
+    return src([
+        "./themes/simple/static/**/*.css",
+    ])
+        .pipe(postcss(plugins))
+        .pipe(dest("output/"));
+};
+
+const buildAll = series(
+    compileCSS
+);
+
+
+const build = series(
+    compileCSS
+);
+exports.build = build;
+
+const pelican = series(build, watchFiles);
+exports.pelican = pelican;
