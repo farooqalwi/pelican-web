98e1b6acd5c8223e52586f4b2cafc817c9439235
Farooq Alwi <farooqalwi313@gmail.com>
2021-08-08 19:00:00 +0500

    working on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,14 @@
-const { src, dest, watch, parallel, series } = require('gulp');
-const postcss = require('gulp-postcss');
-const postcssPresetEnv = require('postcss-preset-env');
+  
+const gulp = require('gulp');
+const cssmin = require('gulp-cssmin');
 
-
-const watchFiles = () => {
-    watch(
-        [
-            "./themes/simple/static/**/*.css"
-        ],
-        { ignoreInitial: false },
-        buildAll
-    );
-};
-
-const compileCSS = () => {
-    const plugins = [
-        // postcssPresetEnv comes with autoprefixer
-        postcssPresetEnv({ stage: 1 })
-    ];
-    return src([
-        "./themes/simple/static/**/*.css",
-    ])
-        .pipe(postcss(plugins))
-        .pipe(dest("output/"));
-};
-
-const buildAll = series(
-    compileCSS
-);
-
-
-const build = series(
-    compileCSS
-);
-exports.build = build;
-
-const pelican = series(build, watchFiles);
-exports.pelican = pelican;
+// Task to minify css using package cssmin
+gulp.task('default', () => {
+     // Folder with files to minify
+     return gulp.src('./css/*.css')
+     //The method pipe() allow you to chain multiple tasks together 
+     //I execute the task to minify the files
+    .pipe(cssmin())
+    //I define the destination of the minified files with the method dest
+    .pipe(gulp.dest('./themes/simple/static/css'));
+});
