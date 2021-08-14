8214173a9e0c1b464549c8be54a4b31e3e2b2f77
Farooq Alwi <farooqalwi313@gmail.com>
2021-08-14 19:05:05 +0500

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,18 +5,18 @@ const concat = require("gulp-concat");
 const postcssPresetEnv = require("postcss-preset-env");
 
 // Task to minify css using package cssmin
-// gulp.task("default", function () {
-//   // Folder with files to minify
-//   return (
-//     gulp
-//       .src("./css/*.css")
-//       //The method pipe() allow you to chain multiple tasks together
-//       //It executes the task to minify the files
-//       .pipe(cssmin())
-//       .pipe(postcss([postcssPresetEnv(/* pluginOptions */)]))
-//       //It concates all css files into one file
-//       .pipe(concat("main.css"))
-//       //It defines the destination of the minified files with the method dest
-//       .pipe(gulp.dest("./static/css"))
-//   );
-// });
+gulp.task("default", function () {
+  // Folder with files to minify
+  return (
+    gulp
+      .src("./css/*.css")
+      //The method pipe() allow you to chain multiple tasks together
+      //It executes the task to minify the files
+      // .pipe(cssmin())
+      .pipe(postcss([postcssPresetEnv(/* pluginOptions */)]))
+      //It concates all css files into one file
+      // .pipe(concat("main.css"))
+      //It defines the destination of the minified files with the method dest
+      .pipe(gulp.dest("./static/css"))
+  );
+});
