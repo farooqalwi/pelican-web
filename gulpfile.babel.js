1fcc7d1e2d3c6e373c7e504e86417f98ac6f4af1
Farooq Alwi <farooqalwi313@gmail.com>
2021-08-14 19:09:35 +0500

    postcss-preset-env used

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,10 +12,11 @@ gulp.task("default", function () {
       .src("./css/*.css")
       //The method pipe() allow you to chain multiple tasks together
       //It executes the task to minify the files
-      // .pipe(cssmin())
-      .pipe(postcss([postcssPresetEnv(/* pluginOptions */)]))
+      .pipe(cssmin())
+      //It converts the css that most browsers can undestand it
+      .pipe(postcss([postcssPresetEnv()]))
       //It concates all css files into one file
-      // .pipe(concat("main.css"))
+      .pipe(concat("main.css"))
       //It defines the destination of the minified files with the method dest
       .pipe(gulp.dest("./static/css"))
   );
