8136bc5c09f456a745649958f1296d6e103dfb0e
Farooq Alwi <farooqalwi313@gmail.com>
2021-08-24 06:52:33 +0500

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,18 +9,20 @@ const browsersync = require("browser-sync").create();
 // Task to minify css using package cssmin
 function cssTasks() {
   // Folder with files to minify
-  return src("./css/*.css")
-    //The method pipe() allow you to chain multiple tasks together
-    //It executes the task to minify the files
-    .pipe(cssmin())
-    //magician generates all @font-face rules. We never have to write a @font-face rule again.
-    .pipe(postcss([magician()]))
-    //RFS is a unit resizing engine which was initially developed to resize font sizes
-    .pipe(postcss([rfs()]))
-    //It concates all css files into one file
-    .pipe(concat("main.css"))
-    //It defines the destination of the minified files with the method dest
-    .pipe(dest("./static/css"))
+  return (
+    src("./css/*.css")
+      //The method pipe() allow you to chain multiple tasks together
+      //It executes the task to minify the files
+      .pipe(cssmin())
+      //magician generates all @font-face rules. We never have to write a @font-face rule again.
+      .pipe(postcss([magician()]))
+      //RFS is a unit resizing engine which was initially developed to resize font sizes
+      .pipe(postcss([rfs()]))
+      //It concates all css files into one file
+      .pipe(concat("main.css"))
+      //It defines the destination of the minified files with the method dest
+      .pipe(dest("./static/css"))
+  );
 }
 
 // browsersyncServe Task
